e7e7b86f67b2e3d4b5b7bf5e8d12160bf3036b4d
Caua-Roberto466 <cauaroberto211@gmail.com>
2025-09-09 22:52:14 -0300

    Index reponsiva

--- JavaScript/menu.js
+++ JavaScript/menu.js
@@ -2,10 +2,10 @@ function mostrar(){
     let menuMobile = document.querySelector(".mobile-menu")
     if (menuMobile.classList.contains('open')){
         menuMobile.classList.remove('open');
-        document.querySelector('.icone').src = "menu_white_36dp.svg"
+        document.querySelector('.icone').src = "../Imagens/menu_white_36dp.svg"
     }else{
         menuMobile.classList.add('open')
-        document.querySelector('.icone').src = "close_white_36dp.svg"
+        document.querySelector('.icone').src = "../Imagens/close_white_36dp.svg"
         menuMobile.style.transition = "0.3s"
     }
 }
--- JavaScript/script.js
+++ JavaScript/script.js
@@ -9,3 +9,15 @@ button.addEventListener('click', () => { //Vai criar uma lista de eventos quando
         button.textContent = 'Modo Noturno'; //Vai deixar o nome modo noturno
     }
 });
+
+const buttonmob = document.getElementById('toggle-dark-mobile');  //Cria a váriavel button para receber o botão do modo noturno
+buttonmob.addEventListener('click', () => { //Vai criar uma lista de eventos quando o botão do modo noturno for clicado
+    document.body.classList.toggle('darkmode'); //vai criar uma classe 'darkmode', que vai ser chamada assim que o botão for clicado
+    
+    // Opcional: trocar texto do botão
+    if (document.body.classList.contains('darkmode')) { //Se quando clicar, o botão já estiver marcado como modo noturno, ele vai mudar para modo claro
+        button.textContent = 'Modo Claro'; //Isso que vai mudar o nome para modo claro
+    } else { //Senão estiver registrado, deixa o texto como modo noturno
+        button.textContent = 'Modo Noturno'; //Vai deixar o nome modo noturno
+    }
+});
