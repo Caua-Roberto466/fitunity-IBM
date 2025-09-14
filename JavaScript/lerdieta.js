7afdd1919075f346b4ed45225bdfb015afc427a5
Caua-Roberto466 <cauaroberto211@gmail.com>
2025-09-14 09:34:44 -0300

    Almoço feito para quem quer ganhar massa

--- JavaScript/lerdieta.js
+++ JavaScript/lerdieta.js
@@ -90,7 +90,7 @@ function esconder5(){
     div_escondida5.style.display = "none"
     lerMais5.style.display = "block"
 }
-/*
+
 var div_escondida6 = document.getElementById("div-escondida6")
 var lerMais6 = document.getElementById("ler-mais6")
 var lerMenos6 = document.getElementById("ler-menos6")
@@ -106,7 +106,7 @@ function esconder6(){
     div_escondida6.style.display = "none"
     lerMais6.style.display = "block"
 }
-
+/*
 var div_escondida7 = document.getElementById("div-escondida7")
 var lerMais7 = document.getElementById("ler-mais7")
 var lerMenos7 = document.getElementById("ler-menos7")
